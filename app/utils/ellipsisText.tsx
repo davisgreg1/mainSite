be9581af20e9cadb34a0c26186fc86fec19271d0
gdavis <gregory.davis@autozone.com>
2022-07-19 19:20:17 -0400

    fix(cms): fixed the blog style for short text with images

--- app/utils/ellipsisText.tsx
+++ app/utils/ellipsisText.tsx
@@ -1,4 +1,9 @@
 const ellipseText = (value: string, length: number): string => {
+  const hasImg = value.includes("<img src=");
+  const hasShortText = value.length < 200;
+  if (hasImg && hasShortText) {
+    return `${value.substring(0, 50).trim()}...`;
+  }
   if (value && value.length <= length) {
     return value;
   }
